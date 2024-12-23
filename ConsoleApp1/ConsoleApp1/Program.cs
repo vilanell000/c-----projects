c2a6f0458842f0bf27093a38b6ea7c4df97861be
vilanell000 <156680267+vilanell000@users.noreply.github.com>
2024-12-23 12:30:57 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CSharpFundamentals
+{
+    public enum Visitfrom
+    {
+        socail = 1,
+        friends = 2,
+        staff = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = Visitfrom.socail;
+
+            switch (source)
+            {
+                case Visitfrom.socail:
+                    Console.WriteLine("aloha~");
+                    break;
+
+                case Visitfrom.friends:
+                    Console.WriteLine("hi bestie~");
+                    break;
+
+                case Visitfrom.staff:
+                    Console.WriteLine(" hello ");
+                    break;
+
+                default:
+                    Console.WriteLine("welcome");
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp2;
+
+internal class Program
+{
+    private static void Main(string[] args, Level Level)
+    {
+        switch (Level)
+        {
+            case Level.Junior:
+                Console.WriteLine("let me get you through...");
+                break;
+            case Level.Beginner:
+                Console.WriteLine("keep plough ahead");
+                break;
+            case Level.Senior:
+                Console.WriteLine("move to next Level");
+                break;
+            default:
+                Console.WriteLine("welcome");
+                break;
+        }
+    }
+}
